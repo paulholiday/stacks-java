a69cef4904ef7d09faa6f9d8a958f749f9c77096
Suresh Krishnan <suresh.krishnan@amido.com>
2020-09-09 22:06:29 +0100

    multimodule - refactoring project into multi module structure

--- java/web/src/main/java/com/xxAMIDOxx/xxSTACKSxx/Application.java
+++ java/web/src/main/java/com/xxAMIDOxx/xxSTACKSxx/Application.java
@@ -2,12 +2,16 @@ package com.xxAMIDOxx.xxSTACKSxx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @PropertySource("classpath:auth.properties")
+@ComponentScan(basePackages = {"com.xxAMIDOxx.*"})
+@EntityScan(basePackages = {"com.xxAMIDOxx.*"})
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
