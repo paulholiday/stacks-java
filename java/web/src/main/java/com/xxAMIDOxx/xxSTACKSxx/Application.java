3ffb874662cbf4317417144af6c65ac6246d9cc5
Suresh Krishnan <suresh.krishnan@amido.com>
2020-09-10 14:15:56 +0100

    multimodule - refactoring project into multi module structure

--- java/web/src/main/java/com/xxAMIDOxx/xxSTACKSxx/Application.java
+++ java/web/src/main/java/com/xxAMIDOxx/xxSTACKSxx/Application.java
@@ -1,19 +1,20 @@
 package com.xxAMIDOxx.xxSTACKSxx;
 
+import com.microsoft.azure.spring.data.cosmosdb.repository.config.EnableCosmosRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @PropertySource("classpath:auth.properties")
 @ComponentScan(basePackages = {"com.xxAMIDOxx.*"})
 @EntityScan(basePackages = {"com.xxAMIDOxx.*"})
-@EnableJpaRepositories(basePackages = {"com.xxAMIDOxx.*"})
+@EnableCosmosRepositories(basePackages = {"com.xxAMIDOxx.*"})
+//@EnableJpaRepositories - to be used
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
